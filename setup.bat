246523194ab7999785461682b56da9318c5b6aa5
asm128 <pabloarielz@outlook.com>
2018-01-27 11:46:58 -0300

    + Added initial files for downloading the other repositories.

---
+++ setup.bat
@@ -0,0 +1,29 @@
+@echo    ///////////////////////////////////////////////////////////////////////
+@echo    //                                                                   //
+@echo    // Base Framework           - https://github.com/asm128/nwol         //
+@echo    // Sample apps over nwol    - https://github.com/asm128/nwol_samples //
+@echo    // Game based in nwol       - https://github.com/asm128/KitsuRPG     //
+@echo    // Course series:                                                    //
+@echo    // - https://github.com/asm128/gpftw                                 //
+@echo    // - https://github.com/asm128/gpftw_advanced                        //
+@echo    // - https://github.com/asm128/gpftw_professional                    //
+@echo    //                                                                   //
+@echo    ///////////////////////////////////////////////////////////////////////
+
+md asm128
+cd asm128
+
+git clone https://github.com/asm128/nwol
+git clone https://github.com/asm128/nwol_samples
+git clone https://github.com/asm128/KitsuRPG
+git clone https://github.com/asm128/gpftw
+git clone https://github.com/asm128/gpftw_advanced
+git clone https://github.com/asm128/gpftw_professional
+git clone https://github.com/asm128/x64.Release
+
+md x64.Debug
+md Win32.Debug
+md Win32.Release
+copy .\x64.Release\*.bmp .\x64.Debug
+copy .\x64.Release\*.bmp .\Win32.Debug
+copy .\x64.Release\*.bmp .\Win32.Release
---
+++ update.bat
@@ -0,0 +1,25 @@
+@cd nwol						
+@git pull		
+@cd ..
+@cd nwol_samples				
+@git pull		
+@cd ..
+@cd KitsuRPG					
+@git pull		
+@cd ..
+@cd gpftw					
+@git pull		
+@cd ..
+@cd gpftw_advanced			
+@git pull		
+@cd ..
+@cd gpftw_professional		
+@git pull		
+@cd ..
+@cd x64.Release				
+@git pull		
+@cd ..
+
+copy .\x64.Release\*.bmp .\x64.Debug
+copy .\x64.Release\*.bmp .\Win32.Debug
+copy .\x64.Release\*.bmp .\Win32.Release
