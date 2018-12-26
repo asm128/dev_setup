3cf91f5e8531a3634f85d2c3eae306545c3c13df
asm128 <pabloarielz@outlook.com>
2018-12-26 11:43:18 -0300

    Updated

--- setup.bat
+++ setup.bat
@@ -1,29 +1,29 @@
-@echo    ///////////////////////////////////////////////////////////////////////
-@echo    //                                                                   //
-@echo    // Base Framework           - https://github.com/asm128/nwol         //
-@echo    // Sample apps over nwol    - https://github.com/asm128/nwol_samples //
-@echo    // Game based in nwol       - https://github.com/asm128/KitsuRPG     //
-@echo    // Course series:                                                    //
-@echo    // - https://github.com/asm128/gpftw                                 //
-@echo    // - https://github.com/asm128/gpftw_advanced                        //
-@echo    // - https://github.com/asm128/gpftw_professional                    //
-@echo    // - https://github.com/asm128/gpftw_expert                          //
-@echo    // - https://github.com/asm128/gpftw_master                          //
-@echo    //                                                                   //
-@echo    // Framework used by gpftw_expert:                                   //
-@echo    // - https://github.com/asm128/lilia                                 //
-@echo    //                                                                   //
-@echo    // Framework used by gpftw_master:                                   //
-@echo    // - https://github.com/asm128/gpk                                   //
-@echo    //                                                                   //
-@echo    // Dependencies required by gpk:                                     //
-@echo    // - https://github.com/asm128/zlib                                  //
-@echo    //                                                                   //
-@echo    // New client-server architecture prototype based on gpk:            //
-@echo    // - https://github.com/gontrill/DoP                                 //
-@echo    //                                                                   //
-@echo    //                                                                   //
-@echo    ///////////////////////////////////////////////////////////////////////
+@echo    ///////////////////////////////////////////////////////////////////////////
+@echo    //                                                                       //
+@echo    // New Framework                - https://github.com/asm128/gpk          //
+@echo    // Framework samples            - https://github.com/asm128/gpk_samples  //
+@echo    // Game prototype based in gpk  - https://github.com/asm128/princess     //
+@echo    //                                                                       //
+@echo    // Framework used by gpftw_master:                                       //
+@echo    // - https://github.com/asm128/gpk                                       //
+@echo    //                                                                       //
+@echo    // Dependencies required by gpk:                                         //
+@echo    // - https://github.com/asm128/zlib                                      //
+@echo    //                                                                       //
+@echo    // Framework used by gpftw_expert:                                       //
+@echo    // - https://github.com/asm128/lilia                                     //
+@echo    //                                                                       //
+@echo    // Old Framework                - https://github.com/asm128/nwol         //
+@echo    // Sample apps over nwol        - https://github.com/asm128/nwol_samples //
+@echo    // Game prototype based in nwol - https://github.com/asm128/KitsuRPG     //
+@echo    // Course series:                                                        //
+@echo    // - https://github.com/asm128/gpftw                                     //
+@echo    // - https://github.com/asm128/gpftw_advanced                            //
+@echo    // - https://github.com/asm128/gpftw_professional                        //
+@echo    // - https://github.com/asm128/gpftw_expert                              //
+@echo    // - https://github.com/asm128/gpftw_master                              //
+@echo    //                                                                       //
+@echo    ///////////////////////////////////////////////////////////////////////////
 
 md asm128
 cd asm128
@@ -39,8 +39,9 @@ git clone https://github.com/asm128/gpftw_expert
 git clone https://github.com/asm128/gpftw_master
 git clone https://github.com/asm128/lilia
 git clone https://github.com/asm128/gpk
-git clone https://github.com/asm128/gpk_android
+git clone https://github.com/asm128/gpk_samples
 git clone https://github.com/asm128/gpk_data
+git clone https://github.com/asm128/gpk_android
 git clone https://github.com/asm128/dwh
 git clone https://github.com/asm128/princess
 
