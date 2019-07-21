aedc7879d8c590ada3293f9dd6f331a6a26caeff
asm128 <pabloarielz@outlook.com>
2019-07-21 00:56:09 -0300

    + Added blitter.bat to download blitter and set up databases.

---
+++ blitter.bat
@@ -0,0 +1,26 @@
+md asm128
+cd asm128
+
+git clone https://github.com/asm128/gpk
+git clone https://github.com/asm128/gpk_data
+git clone https://github.com/asm128/zlib
+git clone https://github.com/asm128/blitter
+
+@cd gpk			
+git pull 
+cd ..
+
+@cd gpk_data	
+git pull 
+cd ..
+
+@cd zlib		
+git pull 
+cd ..
+
+@cd blitter		
+git pull 
+cd ..
+
+cd gpk_data\blitter_db
+all.bat
