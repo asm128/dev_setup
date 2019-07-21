14c7fcbaec7f8790011c4d0a98ba7d7cc78734d9
asm128 <pabloarielz@outlook.com>
2019-07-21 00:51:45 -0300

    + Updated update.bat

--- update.bat
+++ update.bat
@@ -1,53 +1,75 @@
-@git pull
-@cd ..
-@cd nwol_samples
-@git pull
-@cd ..
-@cd KitsuRPG
-@git pull
-@cd ..
-@cd gpftw
-@git pull
-@cd ..
-@cd gpftw_advanced
-@git pull
-@cd ..
-@cd gpftw_professional
-@git pull
-@cd ..
-@cd gpftw_expert
-@git pull
-@cd ..
-@cd gpftw_master
-@git pull
-@cd ..
-@cd lilia
-@git pull
-@cd ..
-@cd gpk
-@git pull
-@cd ..
-@cd gpk_samples
-@git pull
-@cd ..
-@cd gpk_data
-@git pull
-@cd ..
-@cd blitter
-@git pull
-@cd ..
-@cd blade
-@git pull
-@cd ..
-@cd glide
-@git pull
-@cd ..
-@cd gpk_android
-@git pull
-@cd ..
-@cd zlib
-@git pull
-@cd ..
-@cd DoP
-@git pull
-@cd ..
+cd zlib					
+git pull 
+cd ..
+
+cd nwol					
+git pull 
+cd ..
+
+cd nwol_samples			
+git pull 
+cd ..
+
+cd KitsuRPG				
+git pull 
+cd ..
+
+cd gpftw				
+git pull 
+cd ..
+
+cd gpftw_advanced		
+git pull 
+cd ..
+
+cd gpftw_professional	
+git pull 
+cd ..
+
+cd gpftw_expert			
+git pull 
+cd ..
+
+cd gpftw_master			
+git pull 
+cd ..
+
+cd lilia				
+git pull 
+cd ..
+
+cd gpk					
+git pull 
+cd ..
+
+cd gpk_samples			
+git pull 
+cd ..
+
+cd gpk_data				
+git pull 
+cd ..
+
+cd blitter				
+git pull 
+cd ..
+
+cd blade				
+git pull 
+cd ..
+
+cd glide				
+git pull 
+cd ..
+
+cd gpk_android			
+git pull 
+cd ..
+
+cd dwh					
+git pull 
+cd ..
+
+cd princess				
+git pull 
+cd ..
