f3170502c524c913f8a7a6362b3972edf7f74d7c
asm128 <pabloarielz@outlook.com>
2019-07-26 01:28:34 -0300

    - Removed deprecated projects.

--- setup.bat
+++ setup.bat
@@ -2,7 +2,6 @@
 @echo    //                                                                       //
 @echo    // New Framework                - https://github.com/asm128/gpk          //
 @echo    // Framework samples            - https://github.com/asm128/gpk_samples  //
-@echo    // Game prototype based in gpk  - https://github.com/asm128/princess     //
 @echo    //                                                                       //
 @echo    // Framework used by gpftw_master:                                       //
 @echo    // - https://github.com/asm128/gpk                                       //
@@ -13,15 +12,15 @@
 @echo    // Framework used by gpftw_expert:                                       //
 @echo    // - https://github.com/asm128/lilia                                     //
 @echo    //                                                                       //
-@echo    // Old Framework                - https://github.com/asm128/nwol         //
-@echo    // Sample apps over nwol        - https://github.com/asm128/nwol_samples //
-@echo    // Game prototype based in nwol - https://github.com/asm128/KitsuRPG     //
 @echo    // Course series:                                                        //
 @echo    // - https://github.com/asm128/gpftw                                     //
 @echo    // - https://github.com/asm128/gpftw_advanced                            //
 @echo    // - https://github.com/asm128/gpftw_professional                        //
 @echo    // - https://github.com/asm128/gpftw_expert                              //
 @echo    // - https://github.com/asm128/gpftw_master                              //
+@echo    //                                                                       //
+@echo    // Old Framework                - https://github.com/asm128/nwol         //
+@echo    // Sample apps over nwol        - https://github.com/asm128/nwol_samples //
 @echo    //                                                                       //
 @echo    ///////////////////////////////////////////////////////////////////////////
 
@@ -31,20 +30,17 @@ cd asm128
 git clone https://github.com/asm128/zlib
 git clone https://github.com/asm128/nwol
 git clone https://github.com/asm128/nwol_samples
-git clone https://github.com/asm128/KitsuRPG
+git clone https://github.com/asm128/lilia
 git clone https://github.com/asm128/gpftw
 git clone https://github.com/asm128/gpftw_advanced
 git clone https://github.com/asm128/gpftw_professional
 git clone https://github.com/asm128/gpftw_expert
 git clone https://github.com/asm128/gpftw_master
-git clone https://github.com/asm128/lilia
 git clone https://github.com/asm128/gpk
 git clone https://github.com/asm128/gpk_samples
 git clone https://github.com/asm128/gpk_data
 git clone https://github.com/asm128/blitdb
 git clone https://github.com/asm128/blitter
-git clone https://github.com/asm128/bigbro
-git clone https://github.com/asm128/glide
 
 cd blitdb
 all.bat
