ddadf3882372c0a75be3d9c4a0665a713f1e3978
asm128 <pabloarielz@outlook.com>
2020-04-21 11:28:37 -0300

    + Added ced projects.

--- setup.bat
+++ setup.bat
@@ -27,7 +27,7 @@
 md asm128
 cd asm128
 
-git clone https://github.com/asm128/zlib & git clone https://github.com/asm128/nwol & git clone https://github.com/asm128/nwol_samples & git clone https://github.com/asm128/lilia & git clone https://github.com/asm128/gpftw & git clone https://github.com/asm128/gpftw_advanced & git clone https://github.com/asm128/gpftw_professional & git clone https://github.com/asm128/gpftw_expert & git clone https://github.com/asm128/gpftw_master & git clone https://github.com/asm128/gpk & git clone https://github.com/asm128/gpk_samples & git clone https://github.com/asm128/gpk_data & git clone https://github.com/asm128/blitdb & git clone https://github.com/asm128/blitter & git clone https://github.com/asm128/battleground & git clone https://github.com/asm128/klib_renewal & git clone https://github.com/asm128/kitsurpg
+git clone https://github.com/asm128/zlib & git clone https://github.com/asm128/nwol & git clone https://github.com/asm128/nwol_samples & git clone https://github.com/asm128/lilia & git clone https://github.com/asm128/gpftw & git clone https://github.com/asm128/gpftw_advanced & git clone https://github.com/asm128/gpftw_professional & git clone https://github.com/asm128/gpftw_expert & git clone https://github.com/asm128/gpftw_master & git clone https://github.com/asm128/gpk & git clone https://github.com/asm128/gpk_samples & git clone https://github.com/asm128/gpk_data & git clone https://github.com/asm128/blitdb & git clone https://github.com/asm128/blitter & git clone https://github.com/asm128/battleground & git clone https://github.com/asm128/klib_renewal & git clone https://github.com/asm128/kitsurpg & git clone https://github.com/asm128/ced & git clone https://github.com/asm128/ced_data
 
 cd blitdb
 all.bat
