f2620922a1b55f2f9fce6686a39f4d2b222a4292
asm128 <pabloarielz@outlook.com>
2026-09-24 12:10:30 -0300

    Updated setup.bat

--- setup.bat
+++ setup.bat
@@ -27,7 +27,19 @@
 md asm128
 cd asm128
 
-git clone https://github.com/asm128/zlib & git clone https://github.com/asm128/nwol & git clone https://github.com/asm128/nwol_samples & git clone https://github.com/asm128/lilia & git clone https://github.com/asm128/gpftw & git clone https://github.com/asm128/gpftw_advanced & git clone https://github.com/asm128/gpftw_professional & git clone https://github.com/asm128/gpftw_expert & git clone https://github.com/asm128/gpftw_master & git clone https://github.com/asm128/gpk & git clone https://github.com/asm128/gpk_samples & git clone https://github.com/asm128/gpk_data & git clone https://github.com/asm128/blitdb & git clone https://github.com/asm128/blitter & git clone https://github.com/asm128/battleground & git clone https://github.com/asm128/klib_renewal & git clone https://github.com/asm128/kitsurpg & git clone https://github.com/asm128/ced & git clone https://github.com/asm128/ced_data
+REM Array of repositories
+set "gpk_games_repos=battleground blitdb blitter ced ced_data gpftw gpftw_advanced gpftw_expert gpftw_master gpftw_professional gpk gpk_data gpk_samples kitsurpg lilia neutralizer nwol nwol_samples llc llt zlib"
 
+REM Loop through each repository
+for %%i in (%gpk_games_repos%) do (
+	cd %%i
+	git checkout master
+	git pull
+	cd ..
+REM git add %%i
+)
+
+REM git commit -m "Updated submodules to latest heads."
+REM git push
 cd blitdb
 all.bat
